6e06cf06e981e647d7672ca217c229d737935503
Sadik Saroar <91769816+sadiksaroar@users.noreply.github.com>
2025-01-01 22:04:34 +0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:calculator/calculator_scree.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Calculator",
+      theme: ThemeData.dark(),
+      home: const CaculatorScreen(),
+    );
+  }
+}
